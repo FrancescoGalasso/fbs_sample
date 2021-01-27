bddd74c617958f26736639013cc186ac5416ba34
Francesco Galasso <francescogalasso@alfadispenser.com>
2021-01-27 10:46:31 +0100

    added MainWindow(QMainWindow)
    some clean and tidy up with pylint

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,12 +1,42 @@
-from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
+# -*- coding: utf-8 -*-
+
+# pylint: disable=no-name-in-module
+
+""" FBS Sample Project """
 
 import sys
 
+from fbs_runtime.application_context.PyQt5 import ApplicationContext, cached_property
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow
+
+
+class AppContext(ApplicationContext):
+    """ FBS App Context """
+
+    def run(self):
+        self.main_window.show()
+        return self.app.exec_()
+
+    @cached_property
+    def main_window(self):      #pylint: disable=missing-function-docstring
+        return MainWindow(self)
+
+
+class MainWindow(QMainWindow):  # pylint: disable=too-few-public-methods
+    """ Main Window """
+
+    def __init__(self, ctx):
+        super(MainWindow, self).__init__()  # pylint: disable=super-with-arguments
+        self.ctx = ctx
+        self.ui_path = self.ctx.get_resource('main_window.ui')
+        uic.loadUi(self.ui_path, self)
+
+        self.setWindowTitle("FBS Sample Project")
+        self.show()
+
+
 if __name__ == '__main__':
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = QMainWindow()
-    window.resize(250, 150)
-    window.show()
-    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
+    appctxt = AppContext()
+    exit_code = appctxt.run()
     sys.exit(exit_code)
